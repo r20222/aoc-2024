22ec2c0bc18eb3042bfc59adcd967f07d46fa811
Silvano Allemekinders <s.allemekinders@tjip.com>
2024-12-06 13:22:37 +0100

    setup

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+const test = require("./test")
+const readInput = require("./readinput")
+
+module.exports = {
+    test,
+    readInput,
+}
---
+++ src/utils/readinput.js
@@ -0,0 +1,15 @@
+const { sep } = require("path")
+const { readFileSync } = require("fs")
+const getCallerFile = require("get-caller-file")
+
+const readInput = () => {
+    const file = getCallerFile()
+        .split(sep)
+        .slice(0, -1)
+        .concat("input.txt")
+        .join(sep)
+
+    return readFileSync(file).toString()
+}
+
+module.exports = readInput
